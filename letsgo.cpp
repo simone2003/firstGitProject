6a57557a7a239491c73b41149bf4dbae120aa53b
simone2003 <simone.gaioni2003@gmail.com>
2021-03-19 22:49:45 +0100

    added commit from rep2-mach2

--- letsgo.cpp
+++ letsgo.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
+
 using namespace std;
+
 int main()
 {
 cout<< "Git, let's go!!" << endl;
 cout<< "Added line 1" <<endl;
 cout<< "working on a new feature 1 ..." <<endl;
+
 return 0;
 }
-
+/*Added comment from repository 2 (machine 2) */
 
